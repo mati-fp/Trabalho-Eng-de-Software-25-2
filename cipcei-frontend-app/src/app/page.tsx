cf0e27daf648113959aefcfbb12bf4d53b6fdf51
Isaac Bueno <isaacbc42@gmail.com>
2025-11-22 22:53:38 -0300

    fix build

--- cipcei-frontend-app/src/app/page.tsx
+++ cipcei-frontend-app/src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(()=>{
+    const token = localStorage.getItem("auth_token");
+    if(!token){
+      router.push("/login");
+    } 
+  }, [router]);
+  
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800">
       <main className="text-center px-4">

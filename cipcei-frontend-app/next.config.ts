8086f8f56e5ff3aff7dfb3bd46cba2e70f3d97d1
Henrique Trein <trein.lor@gmail.com>
2025-11-26 23:29:50 -0300

    implementação de email

--- cipcei-frontend-app/next.config.ts
+++ cipcei-frontend-app/next.config.ts
@@ -3,15 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // Usa standalone apenas em produção
-  ...(process.env.NODE_ENV === 'production' && { output: 'standalone' }),
-  // Configurações para hot reload no Docker
-  webpackDevMiddleware: (config: any) => {
-    config.watchOptions = {
-      poll: 1000, // Verifica mudanças a cada segundo
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  // ...(process.env.NODE_ENV === 'production' && { output: 'standalone' }),
+
 };
 
 export default nextConfig;
